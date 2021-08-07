0db6ebf219e4e1adf9b80d673c039eb0583cca83
2021-1-RV <ysukata@gmail.com>
2021-08-07 13:17:27 +0300

    second commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-let x1="hello js", x2=100, x3=5.5, x4=true;
+/*let x1="hello js", x2=100, x3=5.5, x4=true;
 const COLOR_RED='red';
 let a=5,h=10;
 let s= 1/2 * a * h;
@@ -9,4 +9,23 @@ console.log(x4);
 console.log(COLOR_RED);
 console.log('площадь треугольника', s);
 alert(`Result is: x1=${x1}, x2=${x2}, x3=${x3}, x4=${x4}, COLOR_RED=${COLOR_RED}`);
-alert(`Result is: ${s}`);
+alert(`Result is: ${s}`);*/
+
+let x1 = prompt("Сколько будет 2+2?");
+if(x1 == 4){
+    console.log(`Верно 2+2 будет равно: ${x1}`);
+}else{
+    console.log("Неверно 2+2 будет равно 4")
+}
+
+let x2 = prompt("Стоимость покупки: ");
+if(x2 == 500 || x2 > 500){
+    x3 = x2 * 0.02;
+    console.log(`Вы получили скидку в 2%, ваша скидка: ${x3}`);
+}else{
+    console.log("Ваша сумма покупки меньше 500 грн");
+}
+
+let x4 =prompt("введите число в гривнах:");
+let x5 =prompt("введите число в копейках:");
+console.log(`${x4} грн. ${x5} коп.`)
